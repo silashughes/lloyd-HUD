8571cc9a3e3e39389b8ac7335939c8b17d8bacbe
Silas Hughes <silas.w.hughes@gmail.com>
2016-11-10 08:59:54 -0500

    wiring works, software serial

--- lloyd-HUD.ino
+++ lloyd-HUD.ino
@@ -1,3 +1,6 @@
+#include <Adafruit_GFX.h>
+#include <gfxfont.h>
+
 /***************************************************
   This is our GFX example for the Adafruit ILI9341 Breakout and Shield
   ----> http://www.adafruit.com/products/1651
@@ -16,7 +19,8 @@
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
-#include "Alltrax.h" //Import the motor controller Library
+#include <SoftwareSerial.h> //Any Software Serial Based implementation needs this
+#include "AlltraxSoftwareSerial.h" //Import the motor controller Library
 
 // For the Adafruit shield, these are the default.
 #define TFT_DC 9
@@ -27,53 +31,50 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 // If using the breakout, change pins as desired
 //Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
-Alltrax controller;
+SoftwareSerial mySerial(4, 5);//Rx, Tx
+AlltraxSoftwareSerial controller(&mySerial);
 
 void setup() {
-  controller.begin(&Serial);
+  Serial.begin(9600);//sets up the arduino to communicate with the computer
+  Serial.println("Starting Screen...");
   tft.begin();
+  tft.fillScreen(ILI9341_BLACK);
 }
 
 
 void loop(void) {
-  for(uint8_t rotation=0; rotation<4; rotation++) {
-    //tft.setRotation(rotation);
-    testText();
-    delay(1000);
-  }
+  textDisplay();
+  delay(50);
 }
 
-unsigned long testFillScreen() {
-  unsigned long start = micros();
-  tft.fillScreen(ILI9341_BLACK);
-  tft.fillScreen(ILI9341_RED);
-  tft.fillScreen(ILI9341_GREEN);
-  tft.fillScreen(ILI9341_BLUE);
-  tft.fillScreen(ILI9341_BLACK);
-  return micros() - start;
-}
-
-unsigned long testText() {
+unsigned long textDisplay() {
   tft.fillScreen(ILI9341_BLACK);
   unsigned long start = micros();
   tft.setCursor(0, 0);
   tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(3);
   tft.println("Voltage: ");
+  //tft.setTextColor(ILI9341_BLACK); tft.write((char)32); tft.setTextColor(ILI9341_WHITE);
+  Serial.println("Voltage: ");
   tft.setTextSize(4);
   tft.print(controller.readVoltage());
+  Serial.println(controller.readVoltage());
   tft.println("V");
   tft.println("");
   tft.setTextSize(2);
   tft.println("Tempurature: ");
+  Serial.println("Tempurature: ");
   tft.setTextSize(3);
   tft.print(controller.readTemp());
-  tft.write(0x00B0); //degree symbol
+  Serial.println(controller.readTemp());
+  tft.write((char)248); //degree symbol
   tft.println("C");
   tft.println("");
   tft.setTextSize(2);
   tft.println("Current: ");
+  Serial.println("Current: ");
   tft.setTextSize(3);
   tft.print(controller.readCurrent());
+  Serial.println(controller.readCurrent());
   tft.println("A");
   tft.println("");
   tft.print(start);
